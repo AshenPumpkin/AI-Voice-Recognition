ea5ed9bcb93908cdc338b0d0247ca88bf633f373
Guy Ben Ari <guybenari123123@gmail.com>
2024-07-24 13:11:18 +0300

    Delete featuresWorker.py

--- featuresWorker.py
+++
@@ -1,9 +0,0 @@
-# here write feature extraction funcs
-import librosa as lb
-
-
-def _featureExtractor(audioFile):
-    sampleRate = lb.get_samplerate(y=audioFile)
-    mel_spectrogram = lb.feature.melspectrogram(y=audioFile, sr=sampleRate)
-    featureArr = [audioFile, sampleRate, mel_spectrogram]
-    return featureArr
